98858b38adf38f5ccd3d044714dcbf9655ab97ca
Al Polinar <alpolinar@gmail.com>
2025-11-17 01:30:27 -0700

    batman

---
+++ apps/server/src/server.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "./app";
+
+const start = async () => {
+  const app = await initializeApp();
+  try {
+    app.listen({ port: 3001 });
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
