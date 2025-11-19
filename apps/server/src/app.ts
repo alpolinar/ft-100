4897a3176d6ddf4dd8aed94aeb85ff14cc0e4ce9
Al Polinar <alpolinar@gmail.com>
2025-11-19 00:33:14 -0700

    add biomejs as formatter in server

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,15 +1,15 @@
 import Fastify from "fastify";
 
 const initializeApp = async () => {
-  const fastify = Fastify({
-    logger: true,
-  });
+    const fastify = Fastify({
+        logger: true,
+    });
 
-  fastify.get("/", (req, res) => {
-    res.send({ hello: "world" });
-  });
-  await fastify.ready();
-  return fastify;
+    fastify.get("/", (req, res) => {
+        res.send({ hello: "world" });
+    });
+    await fastify.ready();
+    return fastify;
 };
 
 export { initializeApp };
